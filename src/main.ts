3912d8997117f44cfcfdea0b3e089c2ed73db5fc
Kirinman <kiri3625@outlook.jp>
2023-05-28 00:52:40 +0900

    feat: aws設定の読み込み

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import './assets/main.css'
-
+import { Amplify } from 'aws-amplify'
+import awsmobile from './aws-exports'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+Amplify.configure(awsmobile)
 
 const app = createApp(App)
 
